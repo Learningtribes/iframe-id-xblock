1878c0355212ee97d5d3a0b731c7c7df2ad6bc21
rythmE <poiuwertion@gmail.com>
2020-09-18 17:49:57 +0800

    To publish completion after 1s after page loaded

--- iframe/static/js/src/iframe.js
+++ iframe/static/js/src/iframe.js
@@ -1,5 +1,17 @@
 /* Javascript for IframeWithAnonymousIDXBlock. */
 function IframeWithAnonymousIDXBlock(runtime, element) {
+
+    var handlerUrl = runtime.handlerUrl(element, 'publish_completion');
+
+    window.onload = function() {
+        setTimeout(function() {
+            var data = {
+                'completion': 1.0,
+            };
+            $.post(handlerUrl, JSON.stringify(data)).complete(function() {});
+        }, 1000);
+    };
+
     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
